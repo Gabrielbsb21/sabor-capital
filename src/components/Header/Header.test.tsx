929bae6539df23f11334d5175b1b64e579ca67ea
gabrielbsb21 <gabrielbsb21@outlook.com>
2024-12-12 10:44:04 -0300

    💥 introduce breaking changes

--- src/components/Header/Header.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Header from '.';
-import React from 'react';
-
-describe('Header', () => {
-  it('renders a heading', () => {
-    render(<Header />);
-  });
-});
